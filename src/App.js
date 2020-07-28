4631ce5b4c6f56d9c40f69d28af3d54fb2fc7566
adam <asoliyev13@gmail.com>
2020-07-27 22:11:14 -0400

    Introduction

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header-background">
+        </div>
+        <div className="header-content">
+          <h1>New York City Hot Spots</h1>
+          <p>Discover restaurants, clubs and bars that are consistently <br />
+          busy and highly rated in Vancouver's most popular areas</p>
+        </div>
       </header>
+
     </div>
   );
 }
